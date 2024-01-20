0c3e4d6c509b3f02db5756a36e1217fcc445e8cf
Mxcatnip-hym <144810883+Mxcatnip-hym@users.noreply.github.com>
2024-01-20 12:55:16 +0800

    Add files via upload

---
+++ week_11/token.py
@@ -0,0 +1,13 @@
+token = ‘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’
+
+t = Github(token)
+user = t.get_user()
+
+followers = user.get_following()
+
+for follower in followers:
+    repos = follower.get_repos()
+    for repo in repos:
+        with open('repos.txt', 'a') as file:
+            file.write(f"{follower.login} : {repo.name}\n")
+            
